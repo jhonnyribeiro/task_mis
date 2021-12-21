dc2b7ec7b6d5152d4e08e1e5b29069ec56761fc8
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-12-21 06:08:35 -0300

    Controllers and routes for admins and users

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin/', 'middleware' => ['role:administrator']], function () {
+    Route::get('dashboard', ['App\Http\Controllers\AdminController', 'dashboard'])->name('admin.dashboard');
+});
+
+Route::group(['prefix' => 'user/', 'middleware' => ['role:director|manager|employee']], function () {
+    Route::get('dashboard', ['App\Http\Controllers\UserController', 'dashboard'])->name('user.dashboard');
+});
