67831d61f5129a6a2ffaec376700c4bcc77644a1
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-12-21 05:50:38 -0300

    install laratrust - Laravel role-based access control package https://laratrust.santigarcor.me/

---
+++ app/Models/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laratrust\Models\LaratrustRole;
+
+class Role extends LaratrustRole
+{
+    use HasFactory;
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use LaratrustUserTrait; // add this trait to your user model
 
     /**
      * The attributes that are mass assignable.
