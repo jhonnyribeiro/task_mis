0a1e43a5897f0a03f831b62e1003c3a2495c432b
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-12-23 06:24:01 -0300

    sidebar in a separated file

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
@@ -76,54 +76,8 @@
     </nav>
 
     <div class="wrapper">
-        <nav id="sidebar">
-            <div class="sidebar-header">
-                <i class="fa fa-user"></i>
-                <h3 class="d-inline m-l-10">Administrator</h3>
-            </div>
 
-            <ul class="list-unstyled components m-l-10">
-                <li>
-                    <a class="active" href="{{ route('admin.dashboard') }}">Dashboard</a>
-                </li>
-                <li>
-                    <a href="#">Manage Users</a>
-                </li>
-                <li>
-                    <a href="#">Manage Permissions</a>
-                </li>
-                <li>
-                    <a href="#">Manage Roles</a>
-                </li>
-                <li>
-                    <a href="#">Manage Departments</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false"
-                       class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </nav>
+        @include('admin.layouts.sidebar')
 
         <div id="content">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
